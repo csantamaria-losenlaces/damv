4bfc8a553b1548f30cdb0ef40bea63bca905c410
Carlos Santamaría <csantamaria.losenlaces@gmail.com>
2023-01-12 12:03:43 +0100

    Segundo commit desde Eclipse de Carlos Santamaría

--- src/Principal.java
+++ src/Principal.java
@@ -3,22 +3,34 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		// Declaración de variables
 		String frase = "";
 		Scanner teclado = new Scanner(System.in);
-		
-		// Entrada de datos con validación que evita valores vacíos
+
+		// Entrada de datos con validación que evita valores vacíos (frase 1)
 		do {
 			System.out.println("Introduce una frase:");
 			frase = teclado.nextLine();
 		} while (frase == "");
-		
-		teclado.close();
-		
+
 		// Salida de datos
-		System.out.println("La frase introducida es: " + frase);	
-	
+		System.out.println("La primera frase introducida es: " + frase);
+
+		// Reiniciamos la variable "frase"
+		frase = "";
+
+		// Entrada de datos con validación que evita valores vacíos (frase 2)
+		do {
+			System.out.println("Introduce una frase:");
+			frase = teclado.nextLine();
+		} while (frase == "");
+
+		// Salida de datos
+		System.out.println("La segunda frase introducida es: " + frase);
+
+		teclado.close();
+
 	}
 
 }
